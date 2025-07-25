89049c5d4c6b6d44d2f9274fa506d749d30d7e46
Gustanol <gualeixos456@gmail.com>
2025-07-25 15:32:11 -0300

    feat: adding a setup script

---
+++ setup.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# __ Environment Variables __ #
+
+DOTFILES_DIR=$(pwd)
+PACKAGES=(foot gsimplecal hyprland waybar mako mpv pipewire wofi)
+KEEP_REPO=$1
+
+# __ Functions __ #
+
+log() {
+    echo -e "\e[1;32m[INFO]\e[0m $1"
+}
+
+erro() {
+    echo -e "\e[1;31m[ERRO]\e[0m $1"
+    exit 1
+}
+
+require_sudo() {
+    if [[ $EUID -ne 0 ]]; then
+        echo "Some stages in this setup will need sudo."
+        sudo -v || exit 1
+    fi
+}
+
+install_packages() {
+    log "[*] Installing some essential packages..."
+    for pkg in "${PACKAGES[@]}"; do
+        if ! command -v "$pkg" >/dev/null 2>&1; then
+            log "[-] Installing $pkg"
+            sudo pacman -S --noconfirm "$pkg"
+        fi
+    done
+}
+
+# create_symlinks() {
+#     log "[*] Creating symlinks"
+#     for dir in hypr foot gsimplecal mako mpv pipewire waybar wofi; do
+#         ln -sf "$DOTFILES_DIR/$dir" ~/.config/
+#     done
+# }
+
+copy_dotfiles() {
+    log "[*] Copying dotfiles"
+    for dir in hypr foot gsimplecal mako mpv pipewire waybar wofi; do
+        cp -r "$DOTFILES_DIR/$dir" ~/.config/
+    done
+
+    if [[ "$KEEP_REPO" != "y" ]]; then
+        log "[*] Removing dotfiles repo"
+        rm -rf "$DOTFILES_DIR"
+    fi
+}
+
+# __ Execution __ #
+
+main() {
+    require_sudo
+    install_packages
+    copy_dotfiles
+    log "[✔] Dotfiles configurated successfully!"
+}
+
+main
